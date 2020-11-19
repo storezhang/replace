b5b5fc1edebc5d61c14f38f9959680d5957ee34e
storezhang <storezhang@gmail.com>
2020-11-19 15:34:21 +0800

    YUNKE-1758 【Server】console更新客户端bug

--- error.go
+++ error.go
@@ -6,5 +6,5 @@ import (
 
 var (
 	// ErrorNotSupportReplace 不支持的文件替换
-	ErrorNotSupportReplace = &gox.CodeError{ErrorCode: 103, Msg: "不支持的文件替换"}
+	ErrorNotSupportReplace = &gox.CodeError{ErrorCode: 103, Message: "不支持的文件替换"}
 )
